aadd2a3c9c4bd8b98b8db5e6ba4f48fe50e3828b
Gustavo Oliveira <gustavo-oa@outlook.com>
2021-03-02 21:42:56 -0300

    remove not used imported component

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,5 @@
 import React  from 'react'
 import {View, Text, StyleSheet, Button } from 'react-native'
-import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
 const GameOverScreen = props => {
     return (
