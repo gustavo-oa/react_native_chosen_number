b1e1898fe280802804a7611330a81fba3255a827
Gustavo Oliveira <gustavo-oa@outlook.com>
2021-03-05 23:45:30 -0300

    initialize with flatlist and add Ionicons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert} from 'react-native';
+import 
+{ 
+    View, 
+    Text, 
+    StyleSheet, 
+    Alert, 
+    ScrollView,
+    FlatList
+} from 'react-native';
 
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
@@ -20,13 +28,13 @@ const generateRandomBetween = (min, max, exclude) => {
 
 
 const GameScreen = props => {
-    const [currentGuess, setCurrentGuess] = useState(
-        generateRandomBetween(1, 100, props.userChoice)
-    );
+    const initialGuess = generateRandomBetween(1, 100, props.userChoice);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
     const [rounds, setRounds] = useState(0);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
     const {userChoice, onGameOver} = props;
+    const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()])
 
 
     useEffect(() => {
@@ -34,6 +42,13 @@ const GameScreen = props => {
             props.onGameOver(rounds);
         }
     }, [currentGuess, userChoice, onGameOver]);
+
+
+    const renderListItem = (listLength, itemData) => (
+        <View key={itemData.item} style={styles.listItem}>
+            <Text>#{listLength - itemData.index}</Text>
+            <Text>{itemData.item}</Text>        
+        </View>)
 
     const nextGuessHandle = direction  => {
     
@@ -45,13 +60,13 @@ const GameScreen = props => {
         if (direction === 'lower') {
             currentHigh.current = currentGuess
         } else {
-            currentLow.current = currentGuess;
+            currentLow.current = currentGuess + 1;
         }
     
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
         setRounds(curRounds => curRounds + 1);
-    
+        setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses] );
     }
 
 
@@ -61,10 +76,22 @@ const GameScreen = props => {
             <NumberContainer>{currentGuess}</NumberContainer>            
             <Card style={styles.buttonContainer}>
                 <MainButton style={styles.lowerButton}  onPress={nextGuessHandle.bind(this,'lower' )} >
-                <Ionicons name="remove" size={24} color="black" />
+                    <Ionicons name="remove-outline" size={24} color="white" />
                 </MainButton>
-                <MainButton  onPress={nextGuessHandle.bind(this,'greater' )} ><Text>Greater</Text> </MainButton>
+                <MainButton  onPress={nextGuessHandle.bind(this,'greater' )} >
+                    <Ionicons name="add-outline" size={24} color="white" />
+                </MainButton>
             </Card>
+            <View style={styles.listContainer}>
+                {/* <ScrollView contentContainerStyle={styles.list}>
+                    {pastGuesses.map((data, index )=> renderListItem(data, pastGuesses.length - index) )}
+                </ScrollView> */}
+                <FlatList 
+                data={pastGuesses} 
+                renderItem={renderListItem.bind(this, pastGuesses.length)} 
+                keyExtractor={(item) => item}></FlatList>
+            </View>
+            
         </View>
     )
 }
@@ -79,8 +106,29 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         marginTop: 20,
-        width: 300,
-        maxWidth: '80%'
+        width: 400,
+        maxWidth: '90%'
+    },
+    listContainer: {
+        flex:1,
+        width: '80%',
+        marginTop: 10
+    },
+    list: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
+    },
+    listItem: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        width: '60%',
+        justifyContent: 'space-around',
+
     },
     lowerButton: {
         backgroundColor: Colors.secondaryColor
